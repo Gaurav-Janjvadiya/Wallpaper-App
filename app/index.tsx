1ca9eee028e91d30d35358c176acd806eed3973d
Gaurav-Janjvadiya <janjvadiyagaurav344@gmail.com>
2025-02-12 19:56:29 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text, Button } from "react-native";
+
+export default function () {
+  return (
+    <View>
+      <Text>Gaurav</Text>
+      <Button title="Hi"></Button>
+    </View>
+  );
+}
